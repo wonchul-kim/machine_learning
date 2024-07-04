f520ab460d953cfa01b60339723ae2938cbd014f
wonchul <kim.wonchul@aiv.ai>
2024-07-04 11:11:23 +0900

    added yolo prediction

--- mlearning/algs/roboflow/ultralytics-main/predict.py
+++ mlearning/algs/roboflow/ultralytics-main/predict.py
@@ -1,15 +1,42 @@
 import glob 
+import os
 import os.path as osp
 from ultralytics import YOLO
 
-# Load a model
-# model = YOLO("yolov8n-obb.pt")  # load an official model
-model = YOLO("/HDD/_projects/github/machine_learning/runs/obb/train8/weights/best.pt")  # load a custom model
+import imgviz
+import json
+from mlearning.utils.vis import vis_seg
 
-# Predict with the model
+weights_file = "/HDD/etc/best.pt"
+model = YOLO(weights_file) 
 
-input_dir = '/Data/01.Image/리치코리아/LA/24.06.21/test/data'
+input_dir = '/DeepLearning/etc/_athena_tests/benchmark/sungwoo/u_gap/split_dataset_yolo_is/images/val'
+output_dir = '/DeepLearning/etc/_athena_tests/benchmark/sungwoo/u_gap/yolo_results'
+
+if not osp.exists(output_dir):
+    os.mkdir(output_dir)
+
 img_files = glob.glob(osp.join(input_dir, '*.bmp'))
 
+preds = {}
 for img_file in img_files:
-    results = model(img_file, save=True, imgsz=1024, conf=0.2)  # predict on an image
+    
+    result = model(img_file, save=False, imgsz=768, conf=0.5)[0]
+    classes = result.boxes.cls.tolist()
+    idx2class = result.names
+    confs = result.boxes.conf.tolist()
+    masks = result.masks
+    
+    color_map = imgviz.label_colormap()[1:len(idx2class) + 1]
+    idx2masks = {}
+    for cls, mask in zip(classes, masks):
+        if cls not in idx2masks:
+            idx2masks[cls] = []
+        idx2masks[cls].append([xy.tolist() for xy in mask.xy])
+    
+    preds.update({img_file: {'idx2masks': idx2masks, 'idx2class': idx2class}})
+    
+    vis_seg(img_file, idx2masks, idx2class, output_dir, color_map)
+    
+with open(osp.join(output_dir, 'preds.json'), 'w', encoding='utf-8') as json_file:
+    json.dump(preds, json_file, ensure_ascii=False, indent=4)
---
+++ mlearning/utils/vis.py
@@ -0,0 +1,44 @@
+import glob 
+import os
+import os.path as osp
+import cv2 
+import numpy as np
+
+def vis_seg(img_file, idx2masks, idx2class, output_dir, color_map):
+    
+    filename = osp.split(osp.splitext(img_file)[0])[-1]
+    img = cv2.imread(img_file)
+    height, width, channel = img.shape
+    vis_mask = np.zeros((height, width, channel))
+    
+    origin = 25, 25
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text1 = np.zeros((50, width, 3), np.uint8)
+    text2 = np.zeros((50, width, 3), np.uint8)
+    text3 = np.zeros((50, 300, 3), np.uint8)
+    cv2.putText(text1, "(a) original", origin, font, 0.6, (255, 255, 255), 1)
+    cv2.putText(text2, "(b) predicted", origin, font, 0.6, (255, 255, 255), 1)
+    cv2.putText(text3, "(c) legend", origin, font, 0.6, (255, 255, 255), 1)
+    
+    for cls, masks in idx2masks.items():
+        for mask in masks:
+            points = np.array(mask, dtype=np.int32)
+            cv2.fillConvexPoly(vis_mask, points, color=tuple(map(int, color_map[int(cls)])))
+            # cv2.fillPoly(vis_mask, [points], color=tuple(map(int, color_map[int(cls)])))
+
+    vis_img = cv2.vconcat([text1, img])
+
+    vis_mask = vis_mask.astype(np.uint8)
+    vis_mask = cv2.addWeighted(img, 0.4, vis_mask, 0.6, 0)
+    vis_mask = cv2.vconcat([text2, vis_mask])
+
+    vis_legend = np.zeros((height, 300, channel), dtype="uint8")
+    for idx, color in enumerate(color_map):
+        color = [int(c) for c in color]
+        cv2.putText(vis_legend, idx2class[idx], (5, (idx * 25) + 17), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.rectangle(vis_legend, (150, (idx * 25)), (300, (idx * 25) + 25), tuple(color), -1)
+    vis_legend = cv2.vconcat([text3, vis_legend])
+
+    vis_res = cv2.hconcat([vis_img, vis_mask, vis_legend])
+
+    cv2.imwrite(osp.join(output_dir, filename + '.png'), vis_res)
