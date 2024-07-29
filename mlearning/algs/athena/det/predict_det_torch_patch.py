d1d8c612be7db02f7a2da8708fe9def6a39eeb22
wonchul <kim.wonchul@aiv.ai>
2024-07-29 16:33:15 +0900

    added patch base det

---
+++ mlearning/algs/athena/det/predict_det_torch_patch.py
@@ -0,0 +1,129 @@
+import glob 
+import os
+import os.path as osp
+import torch
+import imgviz
+import json
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from mlearning.utils.vis.vis_hbb_patch import vis_hbb_patch
+from mlearning.utils.functionals import letterbox
+from athena.src.tasks.detection.frameworks.pytorch.models.yolov7.models.experimental import attempt_load as attempt_load7
+from athena.src.tasks.detection.frameworks.pytorch.models.yolov5.utils.general import check_img_size
+from athena.src.tasks.detection.frameworks.pytorch.models.yolov5.utils.general import (non_max_suppression,
+                                                                                       scale_coords)
+
+compare_mask = True
+imgsz = 1024
+device = 'cuda'
+weights = '/DeepLearning/_projects/sungjin_body/patch/best.pt'
+
+# input_dir = '/Data/01.Image/sungjin_yoke/IMAGE/BODY/24.07.29_미검이미지/w_json/학습'
+# json_dir = '/Data/01.Image/sungjin_yoke/IMAGE/BODY/24.07.29_미검이미지/w_json/학습'
+# output_dir = f'/DeepLearning/_projects/sungjin_body/tests/patch/winter/w_json/학습'
+
+input_dir = '/Data/01.Image/sungjin_yoke/IMAGE/BODY/24.07.29_미검이미지/wo_json/학습'
+json_dir = None
+output_dir = f'/DeepLearning/_projects/sungjin_body/tests/athena_tr_0.1/winter/wo_json/학습'
+
+patch_overlap_ratio = 0.1
+patch_width = imgsz
+patch_height = imgsz
+dx = int((1. - patch_overlap_ratio) * patch_width)
+dy = int((1. - patch_overlap_ratio) * patch_height)
+
+compare_gt = True if json_dir is not None else False
+model = attempt_load7(weights, map_location=device)  # load FP32 model
+imgsz = check_img_size(imgsz, s=model.stride.max())  # check image size
+if isinstance(imgsz, int):
+    imgsz = (imgsz, imgsz)
+model(torch.zeros(1, 3, imgsz[0], imgsz[1]).to(device).type_as(next(model.parameters())))  # run once
+idx2class = {ii: vv for ii, vv in enumerate(model.classes)}
+
+iou_threshold = 0.5
+max_dets = 50
+nms_conf_threshold = 0.25
+nms_iou_threshold = 0.1
+line_width = 5
+font_scale = 2
+classes = model.classes
+
+if not osp.exists(output_dir):
+    os.makedirs(output_dir)
+
+img_files = glob.glob(osp.join(input_dir, '*.bmp'))
+
+results = {}
+compare = {}
+for img_file in tqdm(img_files):
+    filename = osp.split(osp.splitext(img_file)[0])[-1]
+    img = cv2.imread(img_file).astype(np.float32)
+    img_h, img_w, img_c = img.shape
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)/255
+    
+    num_patches = 0
+    idx2xyxys = {}
+    for y0 in range(0, img_h, dy):
+        for x0 in range(0, img_w, dx):
+            num_patches += 1
+            if y0 + patch_height > img_h:
+                # skip if too much overlap (> 0.6)
+                y = img_h - patch_height
+            else:
+                y = y0
+
+            if x0 + patch_width > img_w:
+                x = img_w - patch_width
+            else:
+                x = x0
+
+            xmin, xmax, ymin, ymax = x, x + patch_width, y, y + patch_height
+            input_img, ratio, pad_values = letterbox(img[ymin:ymax, xmin:xmax, :], (imgsz[0], imgsz[1]))
+            
+            torch_img = torch.from_numpy(np.transpose(input_img[np.newaxis, :], (0, 3, 1, 2))).to('cuda')
+            with torch.no_grad():
+                preds = model(torch_img)
+            pred = preds[0].detach().cpu()
+            dets = non_max_suppression(pred, nms_conf_threshold, nms_iou_threshold, None, False)
+            
+            for det in dets:
+                if len(det):
+                    det[:, :4] = scale_coords(img.shape[:2], det[:, :4], (img_h, img_w)).round()
+
+                    for *xyxy, conf, cls in reversed(det):
+                        cls = cls.detach().cpu().item()
+                        conf = float(conf.detach().cpu().item())
+                        if cls not in idx2xyxys.keys():
+                            idx2xyxys[cls] = []
+                            idx2xyxys[cls] = {'bbox': [], 'confidence': []}
+                            
+                        idx2xyxys[cls]['bbox'].append([[int(np.round(xyxy[0].detach().cpu().item()) + xmin), int(np.round(xyxy[1].detach().cpu().item()) + ymin)], 
+                                            [int(np.round(xyxy[2].detach().cpu().item()) + xmin), int(np.round(xyxy[3].detach().cpu().item()) + ymin)]])
+                        idx2xyxys[cls]['confidence'].append(conf)
+        
+    color_map = imgviz.label_colormap()[1:len(idx2class) + 1 + 1]
+    results.update({filename: {'idx2xyxys': idx2xyxys, 'idx2class': idx2class, 'img_file': img_file}})
+    
+    if compare_gt:
+        _compare = vis_hbb_patch(img_file, idx2xyxys, idx2class, output_dir, color_map, json_dir, 
+                        compare_gt=compare_gt, iou_threshold=iou_threshold, line_width=line_width, font_scale=font_scale)
+        _compare.update({"img_file": img_file})
+        compare.update({filename: _compare})
+    else:
+        vis_hbb_patch(img_file, idx2xyxys, idx2class, output_dir, color_map, json_dir, 
+                compare_gt=compare_gt, iou_threshold=iou_threshold, line_width=line_width, font_scale=font_scale)
+    
+with open(osp.join(output_dir, 'preds.json'), 'w', encoding='utf-8') as json_file:
+    json.dump(results, json_file, ensure_ascii=False, indent=4)
+
+if compare_gt:
+    with open(osp.join(output_dir, 'diff.json'), 'w', encoding='utf-8') as json_file:
+        json.dump(compare, json_file, ensure_ascii=False, indent=4)
+    
+    df_compare = pd.DataFrame(compare)
+    df_compare_pixel = df_compare.loc['diff_iou'].T
+    df_compare_pixel.fillna(0, inplace=True)
+    df_compare_pixel.to_csv(osp.join(output_dir, 'diff_iou.csv'))
---
+++ mlearning/utils/vis/vis_hbb_patch.py
@@ -0,0 +1,191 @@
+import os.path as osp
+import cv2 
+import numpy as np
+import warnings
+from mlearning.utils.metrics.iou import get_iou
+from shapely.geometry import (GeometryCollection, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon,
+                              mapping)
+from shapely.ops import polygonize, unary_union
+from copy import deepcopy
+
+def get_text_coords(points, width, height, offset_w=0, offset_h=10):
+    text_coord_x, text_coord_y = int(np.min(points, axis=0)[0]), int(np.min(points, axis=0)[1] - 10)
+    if text_coord_x < offset_w:
+        text_coord_x = int(np.max(points, axis=0)[0] + offset_w)
+        
+    if text_coord_x > width - 100:
+        text_coord_x = 10
+        
+    if text_coord_y < offset_h:
+        text_coord_y = int(np.max(points, axis=0)[1] + offset_h)
+        
+    if text_coord_y > height - 100:
+        text_coord_y = 10
+        
+    return (text_coord_x, text_coord_y)
+
+def handle_self_intersection(points):
+    new_points = []
+    line = LineString([[int(x), int(y)] for x, y in points + [points[0]]])
+
+    polygons = list(polygonize(unary_union(line)))
+
+    if len(polygons) > 1:
+        print("The line is forming polygons by intersecting itself")
+        for polygon in polygons:
+            polygon = [list(item) for item in mapping(polygon)['coordinates'][0]]
+            new_points.append(polygon[:-1])
+    else:
+        return [points]
+
+    return new_points
+
+
+def get_key_by_value(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None
+
+def vis_hbb_patch(img_file, idx2xyxys, idx2class, output_dir, color_map, 
+                  json_dir=None, compare_gt=False, iou_threshold=0.2, line_width=2, font_scale=1):
+    
+    filename = osp.split(osp.splitext(img_file)[0])[-1]
+    img = cv2.imread(img_file)
+    height, width, channel = img.shape
+    vis_hbb = deepcopy(img)
+    if compare_gt:
+        diff_dict = {}
+        points_dict = {'gt': {}, 'pred': {}}
+    vis_gt = None, None
+    
+    origin = 25, 25
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_ori = np.zeros((50, width, 3), np.uint8)
+    text_pred = np.zeros((50, width, 3), np.uint8)
+    text_legends = np.zeros((50, 300, 3), np.uint8)
+    cv2.putText(text_ori, "Original", origin, font, 0.6, (255, 255, 255), 1)
+    cv2.putText(text_pred, "Predicted", origin, font, 0.6, (255, 255, 255), 1)
+    cv2.putText(text_legends, "Legends", origin, font, 0.6, (255, 255, 255), 1)
+    
+    if json_dir is not None:
+        import json
+        json_file = osp.join(json_dir, filename + '.json')
+        vis_gt, vis_compare = deepcopy(img), deepcopy(img)
+        text_gt = np.zeros((50, width, 3), np.uint8)
+        cv2.putText(text_gt, "GT", origin, font, 0.6, (255, 255, 255), 1)
+        text_compare = np.zeros((50, width, 3), np.uint8)
+        cv2.putText(text_compare, "Compare", origin, font, 0.6, (255, 255, 255), 1)
+        
+        with open(json_file, 'r') as jf:
+            anns = json.load(jf)['shapes']
+            
+        for idx, cls in idx2class.items():
+            for ann in anns:
+                label = ann['label']
+                if label != cls:
+                    continue
+                points = ann['points']
+                shape_type = ann['shape_type']
+                
+                if len(points) == 2:
+                    cv2.rectangle(vis_gt, (int(points[0][0]), int(points[0][1])), (int(points[1][0]), int(points[1][1])), 
+                                  tuple(map(int, color_map[-1])), line_width + 1)
+                    cv2.putText(vis_gt, label, get_text_coords(points, width, height), cv2.FONT_HERSHEY_SIMPLEX, font_scale, 
+                                tuple(map(int, color_map[-1])), line_width)
+
+                    if compare_gt:
+                        cv2.rectangle(vis_compare, (int(points[0][0]), int(points[0][1])), (int(points[1][0]), int(points[1][1])),
+                                    tuple(map(int, color_map[-1])), line_width + 1)
+                        cv2.putText(vis_compare, label, get_text_coords(points, width, height), cv2.FONT_HERSHEY_SIMPLEX, font_scale, 
+                                    tuple(map(int, color_map[-1])), line_width)
+                        if label in points_dict['gt']:
+                            points_dict['gt'][label].append(points)
+                        else:
+                            points_dict['gt'].update({label: [points]})
+                else:
+                    raise RuntimeError(f"The points is {points} with {shape_type}")
+                
+        vis_gt = vis_gt.astype(np.uint8)
+        vis_gt = cv2.vconcat([text_gt, vis_gt])
+            
+            
+    for cls, pred in idx2xyxys.items():
+        for xyxy, conf in zip(pred['bbox'], pred['confidence']):
+            points = np.array(xyxy, dtype=np.int32)
+            cv2.rectangle(vis_hbb, (int(points[0][0]), int(points[0][1])), (int(points[1][0]), int(points[1][1])), 
+                                  tuple(map(int, color_map[int(cls)])), line_width)
+            cv2.putText(vis_hbb, f"{idx2class[int(cls)]} {conf:.2f}", get_text_coords(points, width, height), 
+                        cv2.FONT_HERSHEY_SIMPLEX, font_scale, tuple(map(int, color_map[int(cls)])), line_width)
+
+            if compare_gt:
+                cv2.rectangle(vis_compare, (int(points[0][0]), int(points[0][1])), (int(points[1][0]), int(points[1][1])), 
+                                  tuple(map(int, color_map[int(cls)])), line_width)
+                cv2.polylines(vis_compare, [np.array(xyxy, dtype=np.int32)], True,
+                                    tuple(map(int, color_map[int(cls)])), line_width)
+                
+                if idx2class[int(cls)] in points_dict['pred']:
+                    points_dict['pred'][idx2class[int(cls)]].append(points.tolist())
+                else:
+                    points_dict['pred'].update({idx2class[int(cls)]: [points.tolist()]})
+
+    vis_img = cv2.vconcat([text_ori, img])
+
+    vis_hbb = vis_hbb.astype(np.uint8)
+    vis_hbb = cv2.vconcat([text_pred, vis_hbb])
+
+    vis_legend = np.zeros((height, 300, channel), dtype="uint8")
+    for idx, color in enumerate(color_map):
+        color = [int(c) for c in color]
+        cv2.rectangle(vis_legend, (150, (idx * 25)), (300, (idx * 25) + 25), tuple(color), -1)
+        if idx < len(color_map) - 1:
+            cv2.putText(vis_legend, idx2class[idx], (5, (idx * 25) + 17), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        else:
+            cv2.putText(vis_legend, 'GT', (5, (idx * 25) + 17), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        
+    vis_legend = cv2.vconcat([text_legends, vis_legend])
+
+    if vis_gt is None:
+        vis_res = cv2.hconcat([vis_img, vis_hbb, vis_legend])
+    else:
+        if compare_gt:
+            vis_compare = cv2.vconcat([text_compare, vis_compare])
+            vis_res = cv2.hconcat([vis_img, vis_gt, vis_hbb, vis_compare, vis_legend])
+        else:
+            vis_res = cv2.hconcat([vis_img, vis_hbb, vis_legend])
+
+    cv2.imwrite(osp.join(output_dir, filename + '.png'), vis_res)
+    if compare_gt:
+        ious = {}
+        for gt_cls, gt_points in points_dict['gt'].items():
+            for pred_cls, pred_points in points_dict['pred'].items():
+                if gt_cls == pred_cls:
+                    for gt_point in gt_points:
+                        for pred_point in pred_points:
+                            try:
+                                iou = get_iou(gt_point, pred_point)
+                                if iou > iou_threshold:
+                                    if gt_cls in ious:
+                                        ious[gt_cls].append(iou)
+                                    else:
+                                        ious[gt_cls] = [iou]
+                                
+                            except:
+                                _gt_points = handle_self_intersection(gt_point)
+                                
+                                for _gt_point in _gt_points:
+                                    
+                                    _gt_point_4 = [[_gt_point[0][0], _gt_point[0][1]], [_gt_point[1][0], _gt_point[0][1]],
+                                                   [_gt_point[1][0], _gt_point[0][1]], [_gt_point[1][0], _gt_point[1][1]]]
+                                    
+                                    pred_point_4 = [[pred_point[0][0], pred_point[0][1]], [pred_point[1][0], pred_point[0][1]],
+                                                   [pred_point[1][0], pred_point[0][1]], [pred_point[1][0], pred_point[1][1]]]
+                                    
+                                    iou = get_iou(_gt_point_4, pred_point_4)
+                                    if iou > iou_threshold:
+                                        if gt_cls in ious:
+                                            ious[gt_cls].append(iou)
+                                        else:
+                                            ious[gt_cls] = [iou]
+            
+        return {'diff_iou': ious}
