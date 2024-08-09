8fa28b9feeaa51dd7a49a14a7c07fe9cac980c7f
wonchul <kim.wonchul@aiv.ai>
2024-08-09 11:13:47 +0900

    update export

--- mlearning/algs/roboflow/src/export.py
+++ mlearning/algs/roboflow/src/export.py
@@ -1,21 +1,54 @@
 from ultralytics import YOLO
+from mlearning.algs.roboflow.utils.helpers import get_params
 
-
-def export(weights, target):
+def export(weights: str, target: str, params):
+    
+    reload_onnx = False
+    params = get_params(params)
+    if 'input_names' in params and isinstance(params['input_names'], str):
+        params['input_names'] = [name.rstrip().lstrip() for name in params['input_names'].split(',')]
+        reload_onnx = True
+    if 'output_names' in params and isinstance(params['output_names'], str):
+        params['output_names'] = [name.rstrip().lstrip() for name in params['output_names'].split(',')]
+        reload_onnx = True
+        
     model = YOLO(weights)
+    print(f"Successfully LOADED weights: {weights} ")
     
     if target == 'onnx':
-        model.export(format='onnx', opset=14)
+        export2onnx(model, weights, reload_onnx)
     else:
         NotImplementedError(f'There is no such case for {target}')
 
-# model = YOLO('/DeepLearning/_projects/kt_g/outputs/yolov8_obb/train4/weights/yolov8_obb_3328.onnx')
+def export2onnx(model, weights, reload_onnx=False):
+    import onnx
+    model.export(format='onnx', opset=14)
+    onnx_path = weights.replace(".pt", '.onnx')
+    print(f"Saved onnx: {onnx_path}")
+    if reload_onnx:
+        onnx_model = onnx.load(onnx_path)
+        print(f"Successfully LOADED onnx model to change input/output names: {onnx_path} ")
+        
+        if 'input_names' in params:
+            print(f"Start to change input-names for {onnx_path} ")
+            assert len(onnx_model.graph.input) == len(params['input_names']), RuntimeError(f"The number of onnx model input ({len(onnx_model.graph.input)}) must be same to input-names from yaml({len(params['input_names'])})")
+            for idx, (input_tensor, input_name) in enumerate(zip(onnx_model.graph.input, params['input_names'])):
+                print(f"  * {idx} changing {input_tensor.name} to {input_name}")
+                input_tensor.name = input_name
 
-# preds = model('/DeepLearning/_projects/kt_g/split_dataset/val/3_124071716123953_1_image_1.bmp')
+        if 'output_names' in params:
+            print(f"Start to change output names for {onnx_path} ")
+            assert len(onnx_model.graph.output) == len(params['output_names']), RuntimeError(f"The number of onnx model output ({len(onnx_model.graph.output)}) must be same to output-names from yaml({len(params['output_names'])})")
+            for jdx, (output_tensor, output_name) in enumerate(zip(onnx_model.graph.output, params['output_names'])):
+                print(f"  * {jdx} changing {output_tensor.name} to {output_name}")
+                output_tensor.name = output_name
 
+            onnx.save(onnx_model, weights.replace(".pt", '_.onnx'))
+            print(f"Successfully SAVED onnx model: {onnx_path} after changing input/output names")
 
 if __name__ == '__main__':
-    weights = "/HDD/datasets/projects/mlearning/yolo/obb_detection/split_dataset_yolo_obb/outputs/train5/weights/best.pt"
+    weights = "/DeepLearning/_projects/kt_g/wonchul/240807/train4/weights/best.pt"
     target = 'onnx'
+    params = '/HDD/datasets/projects/mlearning/yolo/obb_detection/split_dataset_yolo_obb/export.yaml'
     
-    export(weights, target)
+    export(weights, target, params)
